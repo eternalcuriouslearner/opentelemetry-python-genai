facb6a01332ed872d87ff38918843c99aeb0febd
Radhika Gupta <guptaradhika@microsoft.com>
2026-09-11 23:10:22 +0000

    [opentelemetry-instrumentation-genai-langchain] Capture request model from `ls_model_name` metadata on chat (#682)
    * Capture request model from ls_model_name metadata on chat
    
    * Added CHANGELOG
    
    * Fix lint
    
    * Address feedback
    
    * Remove the unit tests

--- instrumentation/opentelemetry-instrumentation-genai-langchain/src/opentelemetry/instrumentation/genai/langchain/callback_handler.py
+++ instrumentation/opentelemetry-instrumentation-genai-langchain/src/opentelemetry/instrumentation/genai/langchain/callback_handler.py
@@ -283,6 +283,10 @@ class OpenTelemetryLangChainCallbackHandler(BaseCallbackHandler):
                 request_model = str(model)
                 break
 
+        if request_model is None and metadata:
+            if model := metadata.get("ls_model_name"):
+                request_model = str(model)
+
         # Skip telemetry for unsupported request models
         if request_model is None:
             return
